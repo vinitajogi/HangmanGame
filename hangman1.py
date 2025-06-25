2a98d62d66cf1b4315abd26c5899b90f1952ca41
vinitajogi <vinitjogi@hotmail.com>
2025-06-25 13:35:30 +0100

    Create hangman1.py

---
+++ hangman1.py
@@ -0,0 +1,21 @@
+Z= [" o" , "/", "|", "\\", "/", "|", "\\"]
+X = input('enter spell to be guess ')
+x=list(X)
+print(x)
+i=len(x)
+j=len(x)
+T=''
+for alpha in range(len(x)):
+    print(alpha)
+    Y = input('enter character of spelling, 1 character at a time')
+    print(Y)
+    c = 0
+    for beta in range(len(x)):
+        if Y!= x[beta]:
+            c+=1
+            print("unmatch_count",c)
+        if len(x)==c:
+            T+=str(Z[alpha])
+            if str(Z[alpha])==' o' or str(Z[alpha])=="\\":
+                T+="\n"
+        print(T)
